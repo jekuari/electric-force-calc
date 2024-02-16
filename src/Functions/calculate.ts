6b58927fb747bfb9e9815fcaebbb00a09a875775
Ricardo Feregrino <ricardoferegrinojr@gmail.com>
2024-02-16 13:17:51 -0600

    Fixed issues

--- src/Functions/calculate.ts
+++ src/Functions/calculate.ts
@@ -11,11 +11,39 @@ export type Vector = {
 
 export type InRelationTo = "charge1" | "charge2" | "charge3";
 
+export enum MeasureUnits {
+  mm = "mm",
+  cm = "cm",
+  m = "m",
+  km = "km",
+}
+
+export const adjustDistanceToMeters = (
+  distance: number,
+  unit: MeasureUnits,
+): number => {
+  //
+  switch (unit) {
+    case MeasureUnits.mm:
+      //
+      return distance * 1e-3;
+    case MeasureUnits.cm:
+      //
+      return distance * 1e-2;
+    case MeasureUnits.km:
+      //
+      return distance * 1e3;
+    default:
+      return distance;
+  }
+};
+
 export const calculateResultingForce = (
   charge1: Charge,
   charge2: Charge,
   charge3: Charge,
   inRelationTo: InRelationTo,
+  measureUnit: MeasureUnits,
 ): Vector => {
   const charges = [charge1, charge2, charge3];
   let index = 0;
@@ -34,8 +62,8 @@ export const calculateResultingForce = (
       break;
     case "charge3":
       index = 2;
-      index1 = 0;
-      index2 = 1;
+      index1 = 1;
+      index2 = 0;
       break;
   }
   let q1 = charges[index];
@@ -46,9 +74,15 @@ export const calculateResultingForce = (
   q2 = { ...q2, value: adjustToUnits(q2) };
   q3 = { ...q3, value: adjustToUnits(q3) };
 
-  const distanceq1q2 = calculateDistance(q1, q2);
+  const distanceq1q2 = adjustDistanceToMeters(
+    calculateDistance(q1, q2),
+    measureUnit,
+  );
 
-  const distanceq1q3 = calculateDistance(q1, q3);
+  const distanceq1q3 = adjustDistanceToMeters(
+    calculateDistance(q1, q3),
+    measureUnit,
+  );
 
   const forceq1q2 = calculateForce(q1.value, q2.value, distanceq1q2);
 
@@ -58,16 +92,24 @@ export const calculateResultingForce = (
 
   const angleq1q3 = calculateAngle(q1, q3);
 
-  const forceXq1q2 = forceq1q2 * Math.cos(angleq1q2);
+  const forceXq1q2 = Math.abs(forceq1q2 * Math.cos(angleq1q2));
 
-  const forceYq1q2 = forceq1q2 * Math.sin(angleq1q2);
+  const forceYq1q2 = Math.abs(forceq1q2 * Math.sin(angleq1q2));
 
-  const forceXq1q3 = forceq1q3 * Math.cos(angleq1q3);
+  const forceXq1q3 = Math.abs(forceq1q3 * Math.cos(angleq1q3));
 
-  const forceYq1q3 = forceq1q3 * Math.sin(angleq1q3);
+  const forceYq1q3 = Math.abs(forceq1q3 * Math.sin(angleq1q3));
 
-  const forceX = forceXq1q2 + forceXq1q3;
-  const forceY = forceYq1q2 + forceYq1q3;
+  const directionXq1q2 = calculateXdirection(q1, q2);
+
+  const directionYq1q2 = calculateYdirection(q1, q2);
+
+  const directionXq1q3 = calculateXdirection(q1, q3);
+
+  const directionYq1q3 = calculateYdirection(q1, q3);
+
+  const forceX = directionXq1q2 * forceXq1q2 + directionXq1q3 * forceXq1q3;
+  const forceY = directionYq1q2 * forceYq1q2 + directionYq1q3 * forceYq1q3;
 
   const R = Math.sqrt(forceX ** 2 + forceY ** 2);
   const angle = Math.abs(rad2deg(Math.atan(forceY / forceX)));
@@ -93,6 +135,14 @@ const calculateForce = (q1: number, q2: number, r: number): number => {
 
 const calculateAngle = (q1: Charge, q2: Charge): number => {
   return Math.atan2(q2.coords.y - q1.coords.y, q2.coords.x - q1.coords.x);
+};
+
+const calculateXdirection = (q1: Charge, q2: Charge): number => {
+  return q2.coords.x - q1.coords.x < 0 ? -1 : 1;
+};
+
+const calculateYdirection = (q1: Charge, q2: Charge): number => {
+  return q2.coords.y - q1.coords.y < 0 ? -1 : 1;
 };
 
 const adjustToUnits = (q: Charge): number => {
--- src/assets/App/App.tsx
+++ src/assets/App/App.tsx
@@ -1,12 +1,15 @@
 import { useState, type ReactElement, useEffect } from "react";
 import { Charge, ChargeField } from "../../Components/ChargeField/ChargeField";
 import PreRegisterButton from "../../Components/Button/PreRegisterButton";
-import { InRelationTo, Vector, adjustChargeToUnits, calculateResultingForce } from "../../Functions/calculate";
+import {
+  InRelationTo,
+  MeasureUnits,
+  Vector,
+  calculateResultingForce,
+} from "../../Functions/calculate";
 import SimplifiedInputSelect from "../../Components/Forms/SimplifiedInput/SimplifiedInputSelect/SimplifiedInputSelect";
 
-export interface AppProps {}
-
-export default function App(props: AppProps): ReactElement {
+export default function App(): ReactElement {
   const [charge1, setCharge1] = useState<Charge>({
     value: 0,
     coords: { x: 0, y: 0 },
@@ -25,50 +28,58 @@ export default function App(props: AppProps): ReactElement {
     unit: "",
   });
 
-  const [inRelationTo, setInRelationTo] = useState<InRelationTo>("charge1")
+  const [measureUnits, setMeasureUnits] = useState<MeasureUnits>(
+    MeasureUnits.m,
+  );
 
-  const [result, setResult] = useState<Vector | null>()
+  const [inRelationTo, setInRelationTo] = useState<InRelationTo>("charge1");
+
+  const [result, setResult] = useState<Vector | null>();
 
   const handleCalculate = () => {
-    const res = calculateResultingForce(charge1, charge2, charge3, inRelationTo)
-    saveInputsToLocalStorage()
-    setResult(res)
-  }
+    const res = calculateResultingForce(
+      charge1,
+      charge2,
+      charge3,
+      inRelationTo,
+      measureUnits,
+    );
+    saveInputsToLocalStorage();
+    setResult(res);
+  };
 
   const saveInputsToLocalStorage = () => {
-    localStorage.setItem("q1", JSON.stringify(charge1))
-    localStorage.setItem("q2", JSON.stringify(charge2))
-    localStorage.setItem("q3", JSON.stringify(charge3))
-    localStorage.setItem("inRelationTo", inRelationTo)
-  }
+    localStorage.setItem("q1", JSON.stringify(charge1));
+    localStorage.setItem("q2", JSON.stringify(charge2));
+    localStorage.setItem("q3", JSON.stringify(charge3));
+    localStorage.setItem("inRelationTo", inRelationTo);
+  };
 
   useEffect(() => {
-    const q1 = localStorage.getItem("q1")
-    const q1Parsed = q1 && JSON.parse(q1)
+    const q1 = localStorage.getItem("q1");
+    const q1Parsed = q1 && JSON.parse(q1);
 
-    const q2 = localStorage.getItem("q2")
-    const q2Parsed = q2 && JSON.parse(q2)
+    const q2 = localStorage.getItem("q2");
+    const q2Parsed = q2 && JSON.parse(q2);
 
-    const q3 = localStorage.getItem("q3")
-    const q3Parsed = q3 && JSON.parse(q3)
+    const q3 = localStorage.getItem("q3");
+    const q3Parsed = q3 && JSON.parse(q3);
 
-    const inRelationTo = localStorage.getItem("inRelationTo")
+    const inRelationTo = localStorage.getItem("inRelationTo");
 
     if (q1Parsed) {
-      setCharge1(q1Parsed)
+      setCharge1(q1Parsed);
     }
     if (q2Parsed) {
-      setCharge2(q2Parsed)
+      setCharge2(q2Parsed);
     }
     if (q3Parsed) {
-      setCharge3(q3Parsed)
+      setCharge3(q3Parsed);
     }
     if (inRelationTo) {
-      setInRelationTo(inRelationTo as InRelationTo)
+      setInRelationTo(inRelationTo as InRelationTo);
     }
-  }, [])
-    
-
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-4">
@@ -94,16 +105,38 @@ export default function App(props: AppProps): ReactElement {
           key="sda"
           title="Carga 3"
         />
-        <SimplifiedInputSelect title="Calculate in relation to" setter={setInRelationTo} value={inRelationTo} options={["charge1", "charge2", "charge3"]} />
+        <SimplifiedInputSelect
+          title="Calculate in relation to"
+          setter={setInRelationTo}
+          value={inRelationTo}
+          options={["charge1", "charge2", "charge3"]}
+        />
+        <SimplifiedInputSelect
+          title="Measure units"
+          setter={setMeasureUnits}
+          value={measureUnits}
+          options={[
+            MeasureUnits.m,
+            MeasureUnits.cm,
+            MeasureUnits.mm,
+            MeasureUnits.km,
+          ]}
+        />
       </div>
       <PreRegisterButton onClick={handleCalculate}>
         <p>Calculate</p>
       </PreRegisterButton>
       {result && (
-      <div className="max-w-[800px] w-full p-4 bg-white rounded-lg shadow-lg flex flex-col gap-4">
-        <p><strong>m: </strong>{result?.magnitude}</p>
-        <p><strong>a: </strong>{result?.angle}</p>
-      </div>
+        <div className="max-w-[800px] w-full p-4 bg-white rounded-lg shadow-lg flex flex-col gap-4">
+          <p>
+            <strong>m: </strong>
+            {result?.magnitude}
+          </p>
+          <p>
+            <strong>a: </strong>
+            {result?.angle}
+          </p>
+        </div>
       )}
     </div>
   );
